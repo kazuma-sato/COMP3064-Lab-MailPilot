f524707920f1a32494a5825a10565c310557d66a
kazuma-sato <kazuma.sato@georgebrown.ca>
2016-10-06 13:52:43 -0400

    Week 5

--- MailPilot/Assets/Scripts/HUDController.cs
+++ MailPilot/Assets/Scripts/HUDController.cs
@@ -9,10 +9,15 @@ public class HUDController : MonoBehaviour {
 	[SerializeField]
 	Text health = null;
 
+	[SerializeField]
+	GameObject player = null;
+	[SerializeField]
+	GameObject button = null;
 
 	void Start () {
 		points.text = "Points: 0";
 		health.text = "Health: 100";
+
 		Player.Instance.hud = this;
 	}
 
@@ -22,5 +27,15 @@ public class HUDController : MonoBehaviour {
 	}
 	public void UpdateHealth(){
 		health.text = "Health: " + Player.Instance.health;
+	}
+
+	public void GameOver(){
+		points.gameObject.SetActive(false);
+		health.gameObject.SetActive(false);
+		player.SetActive(false);
+
+	}
+	public void ResetGame(){
+		
 	}
 }
--- MailPilot/Assets/Scripts/Player.cs
+++ MailPilot/Assets/Scripts/Player.cs
@@ -2,9 +2,11 @@
 using System.Collections;
 
 public class Player{
+	private const string key = highScore;
+
 	private int _points = 0;
 	private int _health = 100;
-
+	private int _highScore = 0;
 	public HUDController hud;
 
 	private static Player _instance = null;
@@ -14,6 +16,12 @@ public class Player{
 			return _instance;
 		}
 
+	}
+
+	private Player(){
+		if (PlayerPrefs.HasKey (key)) {
+			_highScore = PlayerPrefs.GetInt (key);
+		}
 	}
 
 	public int points { 
@@ -28,6 +36,15 @@ public class Player{
 		set{ 
 			_health = value;
 			hud.UpdateHealth();
+			if(_health <= 0) hud.GameOver();
+		}
+	}
+	public int highScore{
+		get{ return _highScore; }
+		set {
+			if (value > _highScore) {
+				_highScore = value;
+				PlayerPrefs.SetInt(key, value);
 		}
 	}
 
